9d33b288d2a208a7caa4998c3acd856579f2de07
juliecoding <julie.akohler@gmail.com>
2017-11-02 20:51:45 -0600

    Finished through Step 5, woo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const strategy = require('./strategy');
+const request = require('request');
 
 const app = express();
 app.use( session({
@@ -7,6 +10,51 @@ app.use( session({
   resave: false,
   saveUninitialized: false
 }));
+
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use(strategy);
+
+passport.serializeUser( (user, done) => {
+  console.log("USER FROM PASSPORT", user);
+  let { clientID, email, name, followers_url } = user._json;
+  done(JSON.stringify({
+    clientID: clientID,
+    email: email,
+    name: name,
+    //followers_url: followers_url
+  }), null, 4)
+});
+
+passport.deserializeUser( (ob, done) => {
+  done(null, obj);
+});
+
+app.get('/login', passport.authenticate('auth0', {
+    successRedirect: '/followers',
+    failureRedirect: '/login',
+    failureFlash: true,
+    connection: 'github'
+  }
+));
+
+app.get('/followers', (req, res, next) => {
+  //Is there a user on the session?
+  if (req.user) {
+    console.log("USER", user);
+    request({
+      url: req.user.followers,
+      headers: {
+        'User-Agent': req.user.clientID
+      }
+    }, (error, response, body) => {
+      res.status.send(body)
+    })
+  } else {
+    res.redirect('/login');
+  }
+})
+
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
